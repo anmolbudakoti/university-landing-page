acc14d98ad095d28e743d35e595cabeb8a423e42
anmolbudakoti1 <anmol.inventurs@gmail.com>
2025-07-11 12:42:31 +0530

    Fix: remove unnecessary files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,5 @@
 import { Router as WouterRouter, Switch, Route } from "wouter";
 import { useHashLocation } from "wouter/use-hash-location";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/home";
@@ -20,12 +18,10 @@ function Router() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
+    <TooltipProvider>
+      <Toaster />
+      <Router />
+    </TooltipProvider>
   );
 }
 
